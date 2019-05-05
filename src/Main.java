a42e4462ed27bd80bee9ca2cfcc989a526e80c32
dccfmatos <d.catarina.c.f@gmail.com>
2019-05-05 19:12:22 +0100

    Class 43.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(secodsAndMinutes.getDurationString(65,45));
+
+
+
+        System.out.println(secodsAndMinutes.getDurationString(65,70));
         System.out.println(secodsAndMinutes.getDurationString(3945L));
 
     }
--- src/secodsAndMinutes.java
+++ src/secodsAndMinutes.java
@@ -1,5 +1,8 @@
 public class secodsAndMinutes {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+    // it's final so that we can´t change the value of the variable constant)
+
     public static String getDurationString(long minutes, long seconds){
 
         if (minutes >= 0 && seconds >= 0 && seconds <= 59){
@@ -7,10 +10,25 @@ public class secodsAndMinutes {
             long hours = minutes / 60;
             long remainingMinutes = minutes % 60;
 
-            return hours + "h" + remainingMinutes + "m" + seconds + "s";
+            String hoursString = hours + "h";
+            if (hours < 10){
+                hoursString = "0" + hoursString;
+            }
+
+            String minutesString = remainingMinutes + "m";
+            if (remainingMinutes < 10){
+                minutesString = "0" + minutesString;
+            }
+
+            String secondsString = seconds + "s";
+            if (seconds < 10){
+                secondsString = "0" + secondsString;
+            }
+
+            return hoursString + " " + minutesString + " " + secondsString;
 
         }
-        return "Invalid value";
+        return INVALID_VALUE_MESSAGE;
     }
 
     public static String getDurationString(long seconds) {
@@ -23,6 +41,6 @@ public class secodsAndMinutes {
             String x = getDurationString(minutes, remainingSeconds);
             return x;
         }
-        return "Invalid value";
+        return INVALID_VALUE_MESSAGE;
     }
 }
